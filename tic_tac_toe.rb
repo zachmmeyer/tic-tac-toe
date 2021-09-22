278c32bb0a1011e806dc6b87ac02d4e2bed9967b
Zach Meyer <zachary.michael.meyer@gmail.com>
2021-09-22 17:10:53 -0400

    Add initial Board class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,2 +1,14 @@
 # frozen_string_literal: true
 
+# Contains Tic Tac Toe Board
+class Board
+  def initialize
+    @board = Array.new(9) { |i| i + 1 }
+  end
+
+  def draw_board
+    puts "#{@board[0]}|#{@board[1]}|#{@board[2]}\n" \
+         "#{@board[3]}|#{@board[4]}|#{@board[5]}\n" \
+         "#{@board[6]}|#{@board[7]}|#{@board[8]}\n"
+  end
+end
