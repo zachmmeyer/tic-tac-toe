a88b307b34bef91df6f71f21218c03e0ad9f840d
Zach Meyer <zachary.michael.meyer@gmail.com>
2021-09-26 12:53:26 -0400

    Test switch_player method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
-# require '../lib/game'
+require '../lib/game'
 
-# describe Game do
-#
-# end
+describe Game do
+  describe '#switch_player' do
+    it 'switches the current player from Player 1 to Player 2' do
+      @current_player = @player01
+      Game.switch_player
+      expect(@current_player).to eql(@player02)
+    end
+    it 'switches the current player from Player 2 to Player 1' do
+      @current_player = @player02
+      Game.switch_player
+      expect(@current_player).to eql(@player01)
+    end
+  end
+end
