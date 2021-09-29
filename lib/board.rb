5165c143da1e2a5f79c97bcfbe88c4de5080f1e1
Zach Meyer <zachary.michael.meyer@gmail.com>
2021-09-29 19:45:54 -0400

    Give the board a graphical feel

--- lib/board.rb
+++ lib/board.rb
@@ -5,14 +5,18 @@ class Board
   attr_accessor :board
 
   def initialize
-    @board = Array.new(9) { |i| (i + 1).to_s }
+    @board = Array.new(9) { |i| "\e[30;46m #{i + 1} \e[0m" }
   end
 
   def draw_board
     <<~BOARD
-      #{@board[0]}|#{@board[1]}|#{@board[2]}
-      #{@board[3]}|#{@board[4]}|#{@board[5]}
-      #{@board[6]}|#{@board[7]}|#{@board[8]}
+
+      #{@board[0]} #{@board[1]} #{@board[2]}
+
+      #{@board[3]} #{@board[4]} #{@board[5]}
+
+      #{@board[6]} #{@board[7]} #{@board[8]}
+
     BOARD
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,13 +4,20 @@ require '../lib/board'
 
 describe Board do
   describe '#draw_board' do
+    def black_text_blue_bg(input)
+      "\e[30;46m #{input} \e[0m"
+    end
     it 'draws a numbered tic-tac-toe board' do
       board = Board.new
       board_grid =
         <<~BOARD
-          1|2|3
-          4|5|6
-          7|8|9
+
+          #{black_text_blue_bg(1)} #{black_text_blue_bg(2)} #{black_text_blue_bg(3)}
+
+          #{black_text_blue_bg(4)} #{black_text_blue_bg(5)} #{black_text_blue_bg(6)}
+
+          #{black_text_blue_bg(7)} #{black_text_blue_bg(8)} #{black_text_blue_bg(9)}
+
         BOARD
       expect(board.draw_board).to eql(board_grid)
     end
