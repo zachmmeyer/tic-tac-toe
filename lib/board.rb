76cb5988250a028cf53ac0d93935955b439de21e
Zach Meyer <zachary.michael.meyer@gmail.com>
2021-09-29 20:03:06 -0400

    Refactor draw_board method

--- lib/board.rb
+++ lib/board.rb
@@ -4,18 +4,22 @@
 class Board
   attr_accessor :board
 
+  def color_board(input1, input2, input3)
+    "\e[30;46m #{input1} \e[0m \e[30;46m #{input2} \e[0m \e[30;46m #{input3} \e[0m"
+  end
+
   def initialize
-    @board = Array.new(9) { |i| "\e[30;46m #{i + 1} \e[0m" }
+    @board = Array.new(9) { |i| i + 1 }
   end
 
   def draw_board
     <<~BOARD
 
-      #{@board[0]} #{@board[1]} #{@board[2]}
+      #{color_board(@board[0], @board[1], @board[2])}
 
-      #{@board[3]} #{@board[4]} #{@board[5]}
+      #{color_board(@board[3], @board[4], @board[5])}
 
-      #{@board[6]} #{@board[7]} #{@board[8]}
+      #{color_board(@board[6], @board[7], @board[8])}
 
     BOARD
   end
